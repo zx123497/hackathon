4711f61e75320e13fc421bb667091ea8c97336c2
bluebetweenneck <bluebetweenneck@gmail.com>
2021-05-14 22:33:08 +0800

    add 'add office' icon button

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,10 +2,18 @@ import React from "react";
 import Grid from "@material-ui/core/Grid";
 import OfficeCard from "./../components/OfficeCard";
 import { makeStyles } from "@material-ui/core/styles";
+import Fab from "@material-ui/core/Fab";
+import AddIcon from "@material-ui/icons/Add";
+import theme from "./../themes/theme";
 
 const useStyles = makeStyles({
   root: {
     margin: "2% 5%",
+  },
+  fab: {
+    position: "fixed",
+    bottom: theme.spacing(5),
+    right: theme.spacing(5),
   },
 });
 
@@ -27,6 +35,16 @@ const Home = () => {
           </Grid>
         ))}
       </Grid>
+      <Fab
+        aria-label="add"
+        color="primary"
+        onClick={() => {
+          alert("add something");
+        }}
+        className={classes.fab}
+      >
+        <AddIcon />
+      </Fab>
     </div>
   );
 };
