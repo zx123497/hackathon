a531ad5ceeac5333333e15bd59102dad77058afa
leo <leo000111444@gmail.com>
2021-05-15 00:21:44 +0800

    fix: bookshelf hover & model

--- src/pages/Office.js
+++ src/pages/Office.js
@@ -1,11 +1,11 @@
-import React from "react";
+import React,{useState} from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import Box from "@material-ui/core/Box";
 import bookshelf from "./../assets/images/book_hondana_syodana.png";
 import Container from "@material-ui/core/Container";
 import ChatRoom from "./../components/ChatRoom";
 import theme from "./../themes/theme";
-
+import Modal from '../components/Modal/Modal'
 const useStyles = makeStyles({
   root: {},
   media: {
@@ -14,6 +14,11 @@ const useStyles = makeStyles({
   bookshelf: {
     // maxWidth: "300px",
     maxWidth: "50%",
+    "&:hover":{
+        transform: "scale(1.05)",
+        transition: "0.5s",
+        cursor: "pointer"
+    }
   },
   office: {
     position: "relative",
@@ -28,13 +33,38 @@ const useStyles = makeStyles({
 const Office = () => {
   const classes = useStyles();
 
+  const [modalOpenState, setModalOpenState] = useState({
+    open: false,
+});
+
+const handleModalOpen = () => {
+    setModalOpenState({ ...modalOpenState,open: true });
+};
+  const handleBookshelf = ()=>{
+    handleModalOpen();
+  }  
+
+  const handleModalClose = () => {
+    setModalOpenState({
+        open: false,
+        
+    });
+};
+
   return (
     <div className={classes.office}>
       <div className="chatroom">
         <ChatRoom />
       </div>
-      <img className={classes.bookshelf} src={bookshelf} alt="bookshelf" />
+      <img className={classes.bookshelf} src={bookshelf} alt="bookshelf" onClick={handleBookshelf}/>
       welcome to the office.
+      <Modal
+                opened={modalOpenState.open}
+                handleClose={handleModalClose}
+            >
+                <h3 className="title">~檔案櫃~</h3>
+                
+            </Modal>
     </div>
   );
 };
