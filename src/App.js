c1bc05f6379e0b10905b485abe991258fba5e12a
JupiterHsieh <taipeiallen15@gmail.com>
2021-05-14 21:26:44 +0800

    testing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import "./App.css";
 import OfficeCard from "./components/OfficeCard";
+import Navbar from "./components/Navbar";
+
 
 function App() {
   return (
     <div className="App">
       <OfficeCard />
+      <Navbar/>
     </div>
   );
 }
