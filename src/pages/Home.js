e9069a1e6925251aef21415ca83c035a904bd946
leo <leo000111444@gmail.com>
2021-05-15 21:37:18 +0800

    for pulling

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import Box from "@material-ui/core/Box";
 import OfficeCard from "./../components/OfficeCard";
 import { makeStyles } from "@material-ui/core/styles";
 import Fab from "@material-ui/core/Fab";
 import AddIcon from "@material-ui/icons/Add";
 import theme from "./../themes/theme";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
+
 const useStyles = makeStyles({
   root: {
     width: "100%",
@@ -22,8 +23,7 @@ const useStyles = makeStyles({
 
 const Home = () => {
   const classes = useStyles();
-
-  const offices = [
+  const [offices, setOffices] = useState([
     { name: "Office 001", isManager: true },
     { name: "Office 002", isManager: false },
     { name: "Office 003", isManager: false },
@@ -34,7 +34,25 @@ const Home = () => {
     { name: "Office 003", isManager: false },
     { name: "Office 003", isManager: false },
     { name: "Office 003", isManager: false },
-  ];
+  ]);
+  // const offices = [
+  //   { name: "Office 001", isManager: true },
+  //   { name: "Office 002", isManager: false },
+  //   { name: "Office 003", isManager: false },
+  //   { name: "Office 003", isManager: false },
+  //   { name: "Office 003", isManager: false },
+  //   { name: "Office 003", isManager: false },
+  //   { name: "Office 003", isManager: false },
+  //   { name: "Office 003", isManager: false },
+  //   { name: "Office 003", isManager: false },
+  //   { name: "Office 003", isManager: false },
+  // ];
+
+  const handleAddRoom = () => {
+    const new_room = { name: "Office 003", isManager: false };
+    const newArr = offices.concat(new_room);
+    setOffices(newArr);
+  };
 
   return (
     <div className={classes.root}>
@@ -42,16 +60,18 @@ const Home = () => {
         {offices.map((office) => (
           // minWidth有點問題
           <Box p={1} pt={2} minWidth={1 / 5}>
-            <OfficeCard component={Link}  to={'/office'} office={office}></OfficeCard>
+            <OfficeCard
+              component={Link}
+              to={"/office"}
+              office={office}
+            ></OfficeCard>
           </Box>
         ))}
       </Box>
       <Fab
         aria-label="add"
         color="primary"
-        onClick={() => {
-          alert("add something");
-        }}
+        onClick={handleAddRoom}
         className={classes.fab}
       >
         <AddIcon />
