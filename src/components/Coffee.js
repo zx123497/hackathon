6a98753b73584b1e33244efdce6bc74476c67a08
bluebetweenneck <bluebetweenneck@gmail.com>
2021-05-15 22:40:02 +0800

    coffee 50% done

--- src/components/Coffee.js
+++ src/components/Coffee.js
@@ -5,12 +5,19 @@ import coffee0 from "./../assets/images/coffee_0.png";
 import coffee1 from "./../assets/images/coffee_1.png";
 import coffee2 from "./../assets/images/coffee_2.png";
 import coffee3 from "./../assets/images/coffee_3.png";
-import "./../coffee.css";
 import Button from "@material-ui/core/Button";
+import Modal from "@material-ui/core/Modal";
+import { Howl, Howler } from "howler";
+import hotTea from "./../assets/sounds/hot_tea.mp3";
+
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemText from "@material-ui/core/ListItemText";
+import ListItemAvatar from "@material-ui/core/ListItemAvatar";
 
 const useStyles = makeStyles({
   image: {
-    width: 200,
+    width: 250,
   },
   cover: {
     objectFit: "cover",
@@ -24,22 +31,51 @@ const Coffee = (props) => {
 
   const [image, setImage] = useState(coffee0);
 
+  const sound = new Howl({
+    src: [hotTea],
+  });
+
   const handleStartFill = () => {
+    // Play the sound.
+    sound.play();
+
+    // Change global volume.
+    Howler.volume(0.5);
+
     setTimeout(() => {
       setImage(images[1]);
-    }, 1000);
+    }, 1500);
     setTimeout(() => {
       setImage(images[2]);
-    }, 2000);
+    }, 3000);
     setTimeout(() => {
       setImage(images[3]);
-    }, 3000);
+    }, 4500);
   };
 
+  const list = [
+    { rank: 1, name: "John Doe", amount: "1000c.c." },
+    { rank: 2, name: "Jimi Hendrix", amount: "850.c." },
+    { rank: 3, name: "Kurt Kobain", amount: "700c.c." },
+    { rank: 4, name: "Janis Joplin", amount: "500c.c." },
+  ];
+
   return (
-    <div>
-      <img src={image} />
-      <Button onClick={handleStartFill}>Fill</Button>
+    <div display="flex">
+      <div>
+        <List>
+          {list.map((item) => (
+            <ListItem>
+              <ListItemAvatar>{item.rank}</ListItemAvatar>
+              <ListItemText primary={item.name} secondary={item.amount} />
+            </ListItem>
+          ))}
+        </List>
+      </div>
+      <div className={classes.image}>
+        <img className={classes.cover} src={image} />
+        <Button onClick={handleStartFill}>Fill</Button>
+      </div>
     </div>
   );
 };
